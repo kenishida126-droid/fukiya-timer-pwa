dcf3ab548428e8f31aae5cc5662f164bd23e2c66
kenishida126-droid <ken.ishida126@gmail.com>
2026-07-13 07:51:30 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    /fukiya-timer-pwa/service-worker.js
    ========================================================= */
 
-const CACHE_NAME = 'fukiya-timer-pwa-20260712-1';
+const CACHE_NAME = 'fukiya-timer-pwa-20260713-1';
 
 /* --- install 時に一気にキャッシュする対象 ---
    ※ すべて「/fukiya-timer-pwa/」からの絶対パス */
