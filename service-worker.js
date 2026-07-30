31f263aa564d068cebbc331a96524b8f456029f7
kenishida126-droid <ken.ishida126@gmail.com>
2026-07-31 08:18:48 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    /fukiya-timer-pwa/service-worker.js
    ========================================================= */
 
-const CACHE_NAME = 'fukiya-timer-pwa-20260730-5';
+const CACHE_NAME = 'fukiya-timer-pwa-20260731-1';
 
 /* --- install 時に一気にキャッシュする対象 ---
    ※ すべて「/fukiya-timer-pwa/」からの絶対パス */
@@ -22,6 +22,10 @@ const PRECACHE_URLS = [
   '/fukiya-timer-pwa/other/index.html',
   '/fukiya-timer-pwa/gemini/',
   '/fukiya-timer-pwa/gemini/index.html',
+
+  // official images
+  '/fukiya-timer-pwa/official/key_lock.png',
+  '/fukiya-timer-pwa/official/key_unlock.png',
 
   // official mp3
   '/fukiya-timer-pwa/official/start-0.mp3',
