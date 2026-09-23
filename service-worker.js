715e4285702cf73b5cfd48c40fd3e95fe2c2126a
kenishida126-droid <ken.ishida126@gmail.com>
2026-09-23 16:40:00 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    /fukiya-timer-pwa/service-worker.js
    ========================================================= */
 
-const CACHE_NAME = 'fukiya-timer-pwa-20260923-13';
+const CACHE_NAME = 'fukiya-timer-pwa-20260923-14';
 
 const PRECACHE_URLS = [
   '/fukiya-timer-pwa/',
